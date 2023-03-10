f5c9088232bed462d3c40c37dadf259e2f6be2e2
Santosh Pandey <santospandey333@gmail.com>
2023-03-10 12:46:08 +0545

    change frame rate of video

--- source/ffmpeg-cpp/root/build.sh
+++ source/ffmpeg-cpp/root/build.sh
@@ -1,4 +1,7 @@
+#/bin/bash
+
 cd .. && make
 cd root && cp ../build/liba.so .
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/treeleaf/Desktop/ffmpeg-cpp/source/ffmpeg-cpp/root/liba.so
-g++ -o main main.cpp audio-merge.cpp -L. -la -L/home/treeleaf/Desktop/ffmpeg-cpp/ffmpeg -lavformat -lavcodec -lavutil -lswscale -lavfilter
+sudo cp liba.so /usr/lib/
+g++ -o main main.cpp -L. -la -L/home/treeleaf/Desktop/ffmpeg-cpp/ffmpeg -lavformat -lavcodec -lavutil -lswscale -lavfilter
--- source/ffmpeg-cpp/root/main.cpp
+++ source/ffmpeg-cpp/root/main.cpp
@@ -1,60 +1,59 @@
+
 #include <iostream>
-#include <string>
 
 #include "../ffmpeg-cpp/ffmpegcpp.h"
 
-using namespace ffmpegcpp;
 using namespace std;
+using namespace ffmpegcpp;
 
-// This example demonstrates different combinations and usages for ffmpeg-cpp.
-// Configure it by changing the values below.
-// The list of supported codecs is not limited to the ones mentioned below,
-// this is just a list we support in this demo. You can use any codec that is
-// supported by ffmpeg, but you might have to write your own Codec-class wrapper
-// to make it work. Look at the examples in the Codecs-dir to see how it is done.
-void PlayDemo()
+int main()
 {
-    // Create a muxer that will output the video as MP4.
-    Muxer* muxer = new Muxer("jojo.opus");
-
-// Create a MPEG2 codec that will encode the raw data.
-    AudioCodec* codec = new AudioCodec(AV_CODEC_ID_OPUS);
-
-// Create an encoder that will encode the raw audio data using the codec specified above.
-// Tie it to the muxer so it will be written to file.
-    AudioEncoder* encoder = new AudioEncoder(codec, muxer);
-
-// Create a video filter and do some funny stuff with the video data.
-    Filter* filter = new Filter("scale=-1:-1,fps=30", encoder);
-//    Filter* filter = new Filter(" -filter_complex ", encoder);
-
-// Load a container. Pick the best video stream container in the container
-// And send it to the filter.
-    Demuxer* demuxer = new Demuxer("/home/treeleaf/Desktop/sample3.opus");
-    demuxer->DecodeBestAudioStream(filter);
-
-// Prepare the output pipeline.
-// This will decode a small amount of frames so the pipeline can configure itself.
-    demuxer->PreparePipeline();
-
-// Push all the remaining frames through.
-    while (!demuxer->IsDone())
+    // This example will apply some filters to a video and write it back.
+    try
     {
-        demuxer->Step();
+        // Create a muxer that will output the video as MKV.
+        Muxer* muxer = new Muxer("filtered_video.mp4");
+
+        // Create a MPEG2 codec that will encode the raw data.
+        VideoCodec* codec = new VideoCodec(AV_CODEC_ID_MPEG2VIDEO);
+
+        // Set the global quality of the video encoding. This maps to the command line
+        // parameter -qscale and must be within range [0,31].
+        codec->SetQualityScale(0);
+
+        // Create an encoder that will encode the raw audio data as MP3.
+        // Tie it to the muxer so it will be written to the file.
+        VideoEncoder* encoder = new VideoEncoder(codec, muxer);
+
+        // Create a video filter and do some funny stuff with the video data.
+        Filter* filter = new Filter("fps=30", encoder);
+
+        // Load a video from a container and send it to the filter first.
+        Demuxer* demuxer = new Demuxer("/home/treeleaf/Desktop/ffmpeg-cpp/samples/big_buck_bunny.mp4");
+        demuxer->DecodeBestVideoStream(filter);
+
+        // Prepare the output pipeline. This will push a small amount of frames to the file sink until it IsPrimed returns true.
+        demuxer->PreparePipeline();
+
+        // Push all the remaining frames through.
+        while (!demuxer->IsDone())
+        {
+            demuxer->Step();
+        }
+
+        // Save everything to disk by closing the muxer.
+        muxer->Close();
+    }
+    catch (FFmpegException e)
+    {
+        cerr << "Exception caught!" << endl;
+        cerr << e.what() << endl;
+        throw e;
     }
 
-// Save everything to disk by closing the muxer.
-    muxer->Close();
+    cout << "Encoding complete!" << endl;
+    cout << "Press any key to continue..." << endl;
 
+    getchar();
 }
 
-
-int main(int argc, char **argv){
-    ffmpegcpp::AudioCodec *codec = new ffmpegcpp::AudioCodec("mpeg4");
-    int sample_rate = codec->GetDefaultSampleRate();
-    std::cout<<"sample rate "<<sample_rate<<std::endl;
-
-    PlayDemo();
-
-    return 0;
-}
